8d3bc65d5f4e5c5a436941e6e3b7d3a8c0281af0
Vi Tran <contact.vitran@gmail.com>
2021-12-06 19:50:08 +1100

    Code along

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,13 @@ import Container from "./Container";
 function App() {
   return (
     <div className="App">
-      <Container header="Example header" direction="horizontal">
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
+      <Container header="Oprah Giveaway!" direction="vertical">
+        <div>You get a car!</div>
+        <div>And You get a car!</div>
+        <div>And You get a car!</div>
+        <div>EVERYONE GETS A CAR!!</div>
       </Container>
-      <Container header="Example header" textPosition="center">
+      <Container header="I like turtles!" textPosition="right" direction="vertical" contentPosition="left">
         <img
           alt="turtle"
           src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
